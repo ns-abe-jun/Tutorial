a0d68c639123e5132892fb27c17d5a1f45124b9f
Ryuta-Imazu <ryuta.imazu@gmail.com>
2016-03-13 15:20:55 +0900

    test motosugi

--- TestButton/app/src/main/java/com/example/junabe/testbutton/MainActivity.java
+++ TestButton/app/src/main/java/com/example/junabe/testbutton/MainActivity.java
@@ -40,7 +40,8 @@ public class MainActivity extends AppCompatActivity {
                 }
                 // counterを2で割った余りが0以外、つまり1の時
                 else {
-                    textView.setText("World");
+//                    textView.setText("World");
+                      textView.setText("Motosugi");
                 }
             }
         });
